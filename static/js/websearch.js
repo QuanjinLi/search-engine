d870f564ea72d3cf82b3bd24f2e3d072c68f5d59
Quanjin Li <33009519+QuanjinLi@users.noreply.github.com>
2018-04-12 22:22:36 -0700

    Add files via upload

---
+++ static/js/websearch.js
@@ -0,0 +1,10 @@
+// $(document).ready(function () {
+//     $("#search-engine").click(function () {
+//         var search = $(".input").val();
+//         $.post("/webSearch/",{'search':search}, function(ret){
+//             alert(ret);
+//             // $('#result').html(ret.result)
+//         });
+//     });
+//
+// });
